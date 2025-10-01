38f95fea29c8bc2dfb52526f7f4db147f560c7e1
Fahad Shoaib <fahadshoaib15@gmail.com>
2025-10-01 15:13:58 +0500

    added type command

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,9 @@ fn main() {
 
     // Wait for user input
     let mut input = String::new();
-    // let flag: bool = true;
+    let valid_commands = ["echo", "exit", "type"];
+
+
 
     loop {
         io::stdin().read_line(&mut input).unwrap();
@@ -16,17 +18,26 @@ fn main() {
         // let cmd_parts: Vec<&str> = input.split(" ").collect();
         let mut _parts = input.splitn(2, ' ');
         let _cmd = _parts.next().unwrap_or("");
-        let _args = _parts.next().unwrap_or("");
-
+        let _args = _parts.next().unwrap_or("").trim();
 
 
         if _cmd == "exit" {
             break;
         } else if _cmd == "echo" {
-            print!("{_args}");
+            println!("{_args}");
+        } else if _cmd == "type" {
+
+            if valid_commands.contains(&_args) {
+                println!("{_args} is a shell builtin")
+            } else {
+                println!("{_args}: not found");
+            }
+
         } else if input_trimmed != "" {
             println!("{input_trimmed}: command not found");
         }
+
+
 
 
 
